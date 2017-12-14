95338121bbce40c33c4538d759315cf21de523bb
YIMING-PC\Yiming <yimingmichael@gmail.com>
2017-12-14 22:49:14 +0800

    add basic MouseHitEdgeCameraMove

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -4,15 +4,32 @@ public class CameraController : MonoBehaviour {
     public Transform target;
 
     public float smoothSpeed = 10f;
+
     public Vector3 offset;
+
+    public int boundary = 50;
+    public float speed = 5.0f;
+
+    private int screenWidth;
+    private int screenHeight;
 
     // For debug purpose
     public float debugTime;
 
+    private void Start()
+    {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+    }
+
     void FixedUpdate()
     {
         MouseScrollZoom();
+        MouseHitEdgeCameraMove();
+    }
 
+    void CameraFollowsPlayer()
+    {
         Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
         transform.position = smoothedPosition;
@@ -24,5 +41,14 @@ public class CameraController : MonoBehaviour {
     {
         if (Input.GetAxis("Mouse ScrollWheel") > 0) { Camera.main.orthographicSize = Mathf.Max(Camera.main.orthographicSize + 1, 1); }
         if (Input.GetAxis("Mouse ScrollWheel") < 0) { Camera.main.orthographicSize = Mathf.Max(Camera.main.orthographicSize - 1, 1); }
+    }
+
+    // Move camera whenever the mouse hit the boundary of the screen
+    void MouseHitEdgeCameraMove()
+    {
+        if (Input.mousePosition.x > screenWidth - boundary) { transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * speed * Time.deltaTime, 0.0f, 0.0f); }
+        if (Input.mousePosition.x < 0 + boundary) { transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * speed * Time.deltaTime, 0.0f, 0.0f); }
+        if (Input.mousePosition.y > screenHeight - boundary) { transform.position += new Vector3(0.0f, Input.GetAxisRaw("Mouse Y") * speed * Time.deltaTime, 0.0f); }
+        if (Input.mousePosition.y < 0 + boundary) { transform.position += new Vector3(0.0f, Input.GetAxisRaw("Mouse Y") * speed * Time.deltaTime, 0.0f); }
     }
 }
